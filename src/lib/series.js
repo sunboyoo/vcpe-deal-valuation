dbe72c8fa860ae3078b3d05be68a5a687dee5eec
InThePerfectWorld <sunboyoo@gmail.com>
2024-05-05 16:26:02 -0400

    commit

--- src/lib/series.js
+++ src/lib/series.js
@@ -1,27 +1,59 @@
 const example = [
     {
         seriesName: 'Founders',
-        csShares: 5,
-        cpShares: 0,
-        cpFv: 0,
-        rpFv: 0,
-        liqPref: 1,
+        css: 5, // CS Shares
+        cps: 0, // CP Shares
+        cpv: 0, // CP Value
+        rpv: 0, // RP Value
+        rvps: null, // Redeemable Value Per Share
+        conversionOrder: null, // Conversion Order starting from 0
     }
 ]
 
 const seriesArray = []
 
 
-function addSeries(seriesArray, seriesName, csShares, cpShares, cpFv, rpFv, liqPref=1){
-    seriesArray.push({seriesName, csShares, cpShares, cpFv, rpFv, liqPref})
+function addSeries(seriesArray, seriesName, css, cps, cpv, rpv){
+    seriesArray.push({seriesName, css, cps, cpv, rpv, rvps: null, conversionOrder: null})
 }
-
 
 /*
-    Convertible Preferred Shares - Realization Value
- */
-function setCpRv(seriesArray){
+* Set Redeemable Value Per Share
+*/
+function setRvps(seriesArray){
     seriesArray.forEach(series => {
-        series.cpRv = series.cpFv * series.liqPref
+        if (series.cpv > 0 && series.cps > 0){
+            series.rvps = series.cpv / series.cps
+        } else {
+            series.rvps = null // Set to null if not applicable
+        }
     })
 }
+
+/*
+* Assign Conversion Order based on RVPS
+* Adds a 'conversionOrder' property to each series.
+*/
+function assignConversionOrder(seriesArray){
+    // Create a temporary array for sorting and determining order
+    const sortable = seriesArray
+        .map((series, index) => ({index, rvps: series.rvps}))
+        .filter(series => series.rvps > 0)
+
+    // Sort by RVPS
+    sortable.sort((a, b) => a.rvps - b.rvps)
+
+
+    // Assign conversion order starting from 0
+    sortable.forEach((item, order) => {
+        seriesArray[item.index].conversionOrder = order;
+    })
+
+    // Assign null conversion order to series with no CP to indicate they don't convert
+    seriesArray.forEach((series) => {
+        if (!(series.cpv > 0 && series.cps > 0)){
+            series.conversionOrder = null;
+        }
+    })
+}
+
